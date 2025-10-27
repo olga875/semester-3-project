b1210c3d8d2ddabd459f57cb5d98e35c23d6bc2e
olga <ojermakowicz@gmail.com>
2025-10-27 22:45:53 +0100

    basic crud operations, to be joined with auth&ui

--- POC/routes/web.php
+++ POC/routes/web.php
@@ -4,4 +4,5 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HelloController;
 
 Route::get('/', [HelloController::class, 'index'])->name('desk.index');
-Route::post('/', [HelloController::class, 'updateDesk'])->name('desk.update');
+Route::post('/update', [HelloController::class, 'updateDesk'])->name('desk.update');
+Route::post('/save', [HelloController::class, 'updatePreferences'])->name('desk.save');
