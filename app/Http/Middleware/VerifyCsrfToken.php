6f190e63c2f46e7d706ebfd8ce43c20ae79435c9
kleventova <167702951+kleventova@users.noreply.github.com>
2025-11-29 18:20:35 +0100

    removed api.php and amended csrf token page

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -7,12 +7,5 @@ use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken as Middleware;
 class VerifyCsrfToken extends Middleware
 {
     protected $except = [
-        'intervals',
-        'intervals/*',
-
-        'test/select',
-
-        'admin/tables',
-        'admin/tables/*',
     ];
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+<<<<<<< HEAD
 use App\Http\Controllers\AdminController;
 use App->Http->Controllers->HelloController;
 use App->Http->Controllers->PicoController;
@@ -36,10 +37,22 @@ Route::middleware('auth')->group(function () {
     Route::put('/intervals/{interval}', [IntervalController::class, 'update'])->name('intervals.update');
     Route::delete('/intervals/{interval}', [IntervalController::class, 'destroy'])->name('intervals.destroy');
 
+=======
+use App\Http\Controllers\AuthController;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HelloController;
+use App\Http\Middleware\AuthAdminMiddleware;
+use App\Http\Controllers\PreferencesController;
+use App\Http\Controllers\IntervalController;
+use App\Http\Controllers\AdminTableController;
+
+Route::middleware('auth')->group(function () {
+>>>>>>> 145034e (removed api.php and amended csrf token page)
     Route::get('/admin/tables', [AdminTableController::class, 'index'])->name('admin.tables.index');
     Route::post('/admin/tables', [AdminTableController::class, 'store'])->name('admin.tables.store');
     Route::put('/admin/tables/{table}', [AdminTableController::class, 'update'])->name('admin.tables.update');
     Route::delete('/admin/tables/{table}', [AdminTableController::class, 'destroy'])->name('admin.tables.destroy');
+<<<<<<< HEAD
 });
 
 
@@ -59,3 +72,41 @@ Route::middleware(AuthAdminMiddleware::class)->group(function () {
     Route::get('admin/building', [AdminController::class, 'serveBuildings'])->name('admin.building');
     Route::post('/blink', [PicoController::class,'blink'])->name("blink");
 });
+=======
+
+    Route::get('/intervals', [IntervalController::class, 'index'])->name('intervals.index');
+    Route::post('/intervals', [IntervalController::class, 'store'])->name('intervals.store');
+    Route::put('/intervals/{interval}', [IntervalController::class, 'update'])->name('intervals.update');
+    Route::delete('/intervals/{interval}', [IntervalController::class, 'destroy'])->name('intervals.destroy');
+    
+    Route::get('/preferences/customize', [PreferencesController::class, 'customize'])->name('preferences.customize');
+    Route::post('/preferences/customize', [PreferencesController::class, 'saveCustom'])->name('preferences.saveCustom');
+    Route::post('/preferences/preset/{mode}', [PreferencesController::class, 'applyPreset'])->name('preferences.applyPreset');
+
+    Route::get('/', function () {
+        $pref = session('prefs', null);
+        return view('HeightControl', compact('pref'));
+    })->name('home');
+    
+    Route::post('/update-height', [HelloController::class, 'updateDesk'])->name('desk.updateHeight');
+    Route::post('/preferences/sitting', [HelloController::class, 'applySittingHeight'])->name('preferences.sitting');
+    Route::post('/preferences/standing', [HelloController::class, 'applyStandingHeight'])->name('preferences.standing');
+    Route::post('/update', [HelloController::class, 'updateDesk'])->name('desk.update');
+    Route::post('/save', [HelloController::class, 'updateDesk'])->name('desk.save');
+
+    Route::post('admin/{acRequest}', [AuthController::class,'ApproveAccess'])->name('admin.approval');
+});
+
+Route::get('admin', [AuthController::class,'ServeAdmin'])
+    ->middleware(AuthAdminMiddleware::class)
+    ->name('admin.panel');
+
+
+Route::middleware('guest')->group(function () {
+    Route::get('/register', [AuthController::class, 'ServeRegister'])->name('register.form');
+    Route::post("/register", [AuthController::class, "Register"])->name("register");
+
+    Route::get('/login', [AuthController::class, 'ServeLogin'])->name('login.form');
+    Route::post("/login", [AuthController::class, "Login"])->name("login");
+});
+>>>>>>> 145034e (removed api.php and amended csrf token page)
