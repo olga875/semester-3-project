ca0251e75a14a8eee59566f8ba21b6d5a0bda824
kleventova <167702951+kleventova@users.noreply.github.com>
2025-11-30 12:54:26 +0100

    updated web.php

--- routes/web.php
+++ routes/web.php
@@ -1,112 +1,95 @@
 <?php
 
-<<<<<<< HEAD
 use App\Http\Controllers\AdminController;
-use App->Http->Controllers->HelloController;
-use App->Http->Controllers->PicoController;
-use App->Http->Controllers->BookingController;
-use App->Http->Controllers->PreferencesController;
-use App->Http->Controllers->IntervalController;
-use App->Http->Controllers->AdminTableController;
-use App->Http->Middleware->AuthAdminMiddleware;
-use Illuminate->Support->Facades\Route;
-
-Route::middleware('auth')->group(function () {
-    Route::get('/', function () {
-        $pref = session('prefs', null);
-        return view('HeightControl', compact('pref'));
-    })->name('home');
-
-    Route::get('/timetable', function () {
-        return view('Timetable');
-    })->name('timetable');
-    
-    Route::post('/update-height', [HelloController::class, 'updateDesk'])->name('desk.updateHeight');
-    Route::post('/update', [HelloController::class, 'updateDesk'])->name('desk.update');
-    Route::post('/save', [HelloController::class, 'updateDesk'])->name('desk.save');
-
-    Route::get('/preferences/customize', [PreferencesController::class, 'customize'])->name('preferences.customize');
-    Route::post('/preferences/customize', [PreferencesController::class, 'saveCustom'])->name('preferences.saveCustom');
-    Route::post('/preferences/preset/{mode}', [PreferencesController::class, 'applyPreset'])->name('preferences.applyPreset');
-
-    Route::post('/preferences/sitting', [HelloController::class, 'applySittingHeight'])->name('preferences.sitting');
-    Route::post('/preferences/standing', [HelloController::class, 'applyStandingHeight'])->name('preferences.standing');
-    
-    Route::get('/intervals', [IntervalController::class, 'index'])->name('intervals.index');
-    Route::post('/intervals', [IntervalController::class, 'store'])->name('intervals.store');
-    Route::put('/intervals/{interval}', [IntervalController::class, 'update'])->name('intervals.update');
-    Route::delete('/intervals/{interval}', [IntervalController::class, 'destroy'])->name('intervals.destroy');
-
-=======
 use App\Http\Controllers\AuthController;
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HelloController;
 use App\Http\Middleware\AuthAdminMiddleware;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PicoController;
+use App\Http\Controllers\BookingController;
 use App\Http\Controllers\PreferencesController;
 use App\Http\Controllers\IntervalController;
 use App\Http\Controllers\AdminTableController;
 
-Route::middleware('auth')->group(function () {
->>>>>>> 145034e (removed api.php and amended csrf token page)
-    Route::get('/admin/tables', [AdminTableController::class, 'index'])->name('admin.tables.index');
-    Route::post('/admin/tables', [AdminTableController::class, 'store'])->name('admin.tables.store');
-    Route::put('/admin/tables/{table}', [AdminTableController::class, 'update'])->name('admin.tables.update');
-    Route::delete('/admin/tables/{table}', [AdminTableController::class, 'destroy'])->name('admin.tables.destroy');
-<<<<<<< HEAD
-});
+Route::get('/', function () {
+    return view('HeightControl');
+})->middleware('auth')->name('home');
 
+Route::get('/timetable', function () {
+    return view('Timetable');
+})->middleware('auth')->name('timetable');
 
-Route::middleware('guest')->group(function () {
-    Route::get('/register', [AuthController::class, 'ServeRegister'])->name('register.form');
-    Route::post('/register', [AuthController::class, 'Register'])->name('register');
+// Desk movement endpoint used by front-end
+Route::post('/update-height', [HelloController::class, 'updateDesk'])->middleware('auth')->name('desk.updateHeight');
 
-    Route::get('/login', [AuthController::class, 'ServeLogin'])->name('login.form');
-    Route::post('/login', [AuthController::class, 'Login'])->name('login');
-});
+// Preferences save endpoints
+Route::post('/preferences/sitting', [HelloController::class, 'applySittingHeight'])->middleware('auth')->name('preferences.sitting');
+Route::post('/preferences/standing', [HelloController::class, 'applyStandingHeight'])->middleware('auth')->name('preferences.standing');
 
+// Map update-height endpoint to the existing updateDesk method
+Route::post('/update', [HelloController::class, 'updateDesk'])->middleware('auth')->name('desk.update');
+Route::post('/save', [HelloController::class, 'updateDesk'])->middleware('auth')->name('desk.save');
 
-Route::middleware(AuthAdminMiddleware::class)->group(function () {
-    Route::get('admin', [AuthController::class, 'ServeAdmin'])->name('admin.panel');
-    Route::post('admin/{acRequest}', [AuthController::class, 'ApproveAccess'])->name('admin.approval');
-    
-    Route::get('admin/building', [AdminController::class, 'serveBuildings'])->name('admin.building');
-    Route::post('/blink', [PicoController::class,'blink'])->name("blink");
-});
-=======
+Route::get('/preferences/customize', [PreferencesController::class, 'customize'])
+    ->middleware('auth')
+    ->name('preferences.customize');
 
-    Route::get('/intervals', [IntervalController::class, 'index'])->name('intervals.index');
-    Route::post('/intervals', [IntervalController::class, 'store'])->name('intervals.store');
-    Route::put('/intervals/{interval}', [IntervalController::class, 'update'])->name('intervals.update');
-    Route::delete('/intervals/{interval}', [IntervalController::class, 'destroy'])->name('intervals.destroy');
-    
-    Route::get('/preferences/customize', [PreferencesController::class, 'customize'])->name('preferences.customize');
-    Route::post('/preferences/customize', [PreferencesController::class, 'saveCustom'])->name('preferences.saveCustom');
-    Route::post('/preferences/preset/{mode}', [PreferencesController::class, 'applyPreset'])->name('preferences.applyPreset');
+Route::post('/preferences/customize', [PreferencesController::class, 'saveCustom'])
+    ->middleware('auth')
+    ->name('preferences.saveCustom');
 
-    Route::get('/', function () {
-        $pref = session('prefs', null);
-        return view('HeightControl', compact('pref'));
-    })->name('home');
-    
-    Route::post('/update-height', [HelloController::class, 'updateDesk'])->name('desk.updateHeight');
-    Route::post('/preferences/sitting', [HelloController::class, 'applySittingHeight'])->name('preferences.sitting');
-    Route::post('/preferences/standing', [HelloController::class, 'applyStandingHeight'])->name('preferences.standing');
-    Route::post('/update', [HelloController::class, 'updateDesk'])->name('desk.update');
-    Route::post('/save', [HelloController::class, 'updateDesk'])->name('desk.save');
+Route::post('/preferences/preset/{mode}', [PreferencesController::class, 'applyPreset'])
+    ->middleware('auth')
+    ->name('preferences.applyPreset');
 
-    Route::post('admin/{acRequest}', [AuthController::class,'ApproveAccess'])->name('admin.approval');
-});
+Route::get('/intervals', [IntervalController::class, 'index'])
+    ->middleware('auth')
+    ->name('intervals.index');
 
-Route::get('admin', [AuthController::class,'ServeAdmin'])
+Route::post('/intervals', [IntervalController::class, 'store'])
+    ->middleware('auth')
+    ->name('intervals.store');
+
+Route::put('/intervals/{interval}', [IntervalController::class, 'update'])
+    ->middleware('auth')
+    ->name('intervals.update');
+
+Route::delete('/intervals/{interval}', [IntervalController::class, 'destroy'])
+    ->middleware('auth')
+    ->name('intervals.destroy');
+
+Route::get('/admin/tables', [AdminTableController::class, 'index'])
+    ->middleware('auth')
+    ->name('admin.tables.index');
+
+Route::post('/admin/tables', [AdminTableController::class, 'store'])
+    ->middleware('auth')
+    ->name('admin.tables.store');
+
+Route::put('/admin/tables/{table}', [AdminTableController::class, 'update'])
+    ->middleware('auth')
+    ->name('admin.tables.update');
+
+Route::delete('/admin/tables/{table}', [AdminTableController::class, 'destroy'])
+    ->middleware('auth')
+    ->name('admin.tables.destroy');
+
+Route::get('/register', [AuthController::class, 'ServeRegister'])->middleware('guest')->name('register.form');
+Route::post('/register', [AuthController::class, 'Register'])->middleware('guest')->name('register');
+
+Route::get('/login', [AuthController::class, 'ServeLogin'])->middleware('guest')->name('login.form');
+Route::post('/login', [AuthController::class, 'Login'])->middleware('guest')->name('login');
+
+Route::get('admin', [AuthController::class, 'ServeAdmin'])
     ->middleware(AuthAdminMiddleware::class)
     ->name('admin.panel');
 
+Route::post('admin/{acRequest}', [AuthController::class, 'ApproveAccess'])
+    ->middleware(AuthAdminMiddleware::class)
+    ->name('admin.approval');
 
-Route::middleware('guest')->group(function () {
-    Route::get('/register', [AuthController::class, 'ServeRegister'])->name('register.form');
-    Route::post("/register", [AuthController::class, "Register"])->name("register");
+Route::get('admin/building', [AdminController::class, 'serveBuildings'])
+    ->middleware(AuthAdminMiddleware::class)
+    ->name('admin.building');
 
-    Route::get('/login', [AuthController::class, 'ServeLogin'])->name('login.form');
-    Route::post("/login", [AuthController::class, "Login"])->name("login");
-});
->>>>>>> 145034e (removed api.php and amended csrf token page)
+Route::post('/blink', [PicoController::class,'blink'])->name("blink");
