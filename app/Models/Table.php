1677d2128672a3fa78afb63ea80cf3e9f37eb586
kleventova <167702951+kleventova@users.noreply.github.com>
2025-11-30 14:12:24 +0100

    removed github conflict comment
    Updated fillable properties for Table model.

--- app/Models/Table.php
+++ app/Models/Table.php
@@ -6,27 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Table extends Model
 {
-<<<<<<< HEAD
-<<<<<<< HEAD
-    //use HasFactory;
-
-    protected $fillable = [
-        'current_height',
-        'location_building',
-        'location_room',
-        'name'
-    ];
-}
-
-// still needs the things given by the API added
-=======
-    //
-=======
     protected $fillable = [
         'name',
         'floor',
         'is_active'
     ];
->>>>>>> 3966384 (task three CRUD for admin)
+
+    // still needs the things given by the API added
 }
->>>>>>> 289fc06 (table selection logic ready and tested)
