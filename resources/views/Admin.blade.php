80ff6fc593584ec04354235f386bd88ceb5b5159
olga <ojermakowicz@gmail.com>
2025-12-04 10:20:05 +0100

    navigation for admin

--- resources/views/Admin.blade.php
+++ resources/views/Admin.blade.php
@@ -10,23 +10,36 @@
 @endpush
 
 @section('content')
-    <section class="filters">
-        <h1>Options</h1>
-        <a>Building Control</a>
-    </section>
-    <section class="list">
-        @foreach ($requests as $rec)
-            <div class="request"  data-id="{{ $rec->id }}">
-                <p>{{ $rec->id }}</p>
-                <p>{{ $rec->user->name }}</p>
-                <p>{{ $rec->user->email  }}</p>
-                <P>{{ $rec->created_at }}</p>
-                <p>{{ $rec->level }}</p>
-                <div class="actions">
-                    <button id="ban"><i class="bi bi-ban"></i></button>
-                    <button id="approve"><i class="bi bi-check-circle"></i></button>
-                </div>
-            </div>
-        @endforeach
-    </section>
+    <header class="app-bar">
+        <h1>Employee Dashboard</h1>
+        <div class="nav-buttons">
+            <form method="GET" action="{{route('logout')}}">
+                <button type="submit">Logout</button>
+            </form>
+        </div>
+    </header>
+
+    <main>
+        <section class="filters">
+            <h1>Options</h1>
+            <a>Building Control</a>
+        </section>
+        <section class="list">
+            @if (isset($requests) && is_iterable($requests))
+                @foreach ($requests as $rec)
+                    <div class="request"  data-id="{{ $rec->id }}">
+                        <p>{{ $rec->id }}</p>
+                        <p>{{ $rec->user->name }}</p>
+                        <p>{{ $rec->user->email  }}</p>
+                        <P>{{ $rec->created_at }}</p>
+                        <p>{{ $rec->level }}</p>
+                        <div class="actions">
+                            <button id="ban"><i class="bi bi-ban"></i></button>
+                            <button id="approve"><i class="bi bi-check-circle"></i></button>
+                        </div>
+                    </div>
+                @endforeach
+            @endif
+        </section>
+    </main>
 @endsection
